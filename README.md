54c44854f1f018ab363b47f5b4fef57aaea32605
BenitoMF <b.martinez2@sapalomera.cat>
2024-04-05 17:41:59 +0300

    Documentacio

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Per la part de Oauth ho he fet per localhost:8000 perque en el moment de posar el link de la pàgina no hem deixava posar practica07.test,
-aleshores el projecte s'ha d'engegar per terminal del Visual Studio mateix amb les comandes:
+aleshores el projecte s'ha d'engegar per 2 terminals del Visual Studio amb les comandes corresponents:
 - php artisan serve
 - npm run dev
+
+I obrir-lo per localhost:8000
 
--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -10,16 +10,32 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class ProviderController extends Controller
-{
+{    
+    /**
+     * redirect
+     * S'encarrega de redirigir a l'usuari a la pàgina 
+     * d'inici de sessió corresponent
+     * @param  mixed $provider
+     * @return void
+     */
     public function redirect($provider){
 
         return Socialite::driver($provider)->redirect();
     }
-
+    
+    /**
+     * callback
+     * Agafa les dades del usuari per fer l'inici de sessió.
+     * Si no existeix l'inserta a la base de dades
+     * Si aquest usuari inicia sessió amb un metode diferent el notifica. 
+     * @param  mixed $provider
+     * @return void
+     */
     public function callback($provider){
         try {
 
             $SocialUser = Socialite::driver($provider)->user();
+
             if(User::where('email',$SocialUser->getEmail())->exists()){
                 return redirect('/login')->withErrors(['email' => 'El compte de correu utilitza un mètode diferent']);
             }
@@ -33,7 +49,7 @@ class ProviderController extends Controller
             return redirect('/login');
         }
 
-       $SocialUser = Socialite::driver($provider)->user();
+
        $user = User::updateOrCreate([
         'provider_id' => $SocialUser->id,
         'provider' => $provider
